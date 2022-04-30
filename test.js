be1b8dea647302a39c4540c96e1ec6241f459507
Carolina Quezada <92093754+carolinaqzada@users.noreply.github.com>
2022-04-30 08:24:24 -0400

    Third branch with deleted line

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 //This is a test. May I add folders to keep this organized?
 //This is my fourth edit, on line number three. Im going to delete my first edit
-//This is my second edit because I forgot to add a note to my first edit
+//This is 3rd branch with a problem
 //What happens to that first note when I create another edit with another note?
 //This is my first edit on a new branch. I can see the test-branch name in the file. 
 //This is 2nd branch no probem
