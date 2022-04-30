ce4b68b41e4a1494ded35a8e403e84204126999b
Carolina Quezada <92093754+carolinaqzada@users.noreply.github.com>
2022-04-30 07:53:22 -0400

    My fourth edit. Removed a line

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 //This is a test. May I add folders to keep this organized?
-//This is my first edit on a file
+//This is my fourth edit, on line number three. Im going to delete my first edit
 //This is my second edit because I forgot to add a note to my first edit
 //What happens to that first note when I create another edit with another note?
